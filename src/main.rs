01269210311123742e60ac71d016740530597472
Keith Noguchi <keith@noguchi.us>
2023-03-13 23:27:45 -0700

    Move unit test to test.rs module (#12)

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::fmt;
 
 use tracing::{debug, instrument, trace};
 
+#[cfg(test)]
+mod test;
+
 #[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
 struct Vertex(char);
 
@@ -117,68 +120,6 @@ impl Dex {
         }
 
         best_rate
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::Dex;
-
-    #[test]
-    fn test_direct() {
-        let mut dex = Dex::new();
-        dex.add_rate('A', 'B', 1.4);
-        dex.add_rate('A', 'C', 0.29);
-        dex.add_rate('B', 'C', 0.2);
-
-        let src = 'A'.into();
-        let dst = 'C'.into();
-        let rate = dex.find_best_rate(&src, &dst);
-        assert_eq!(rate, Some(0.29));
-    }
-
-    #[test]
-    fn test_one_hop() {
-        let mut dex = Dex::new();
-        dex.add_rate('A', 'B', 1.4);
-        dex.add_rate('A', 'C', 0.1);
-        dex.add_rate('B', 'C', 0.2);
-
-        let src = 'A'.into();
-        let dst = 'C'.into();
-        let rate = dex.find_best_rate(&src, &dst);
-        assert_eq!(rate, Some(0.28));
-    }
-
-    #[test]
-    fn test_two_hops() {
-        let mut dex = Dex::new();
-        dex.add_rate('A', 'B', 1.4);
-        dex.add_rate('A', 'C', 0.1);
-        dex.add_rate('A', 'D', 0.055);
-        dex.add_rate('B', 'C', 0.2);
-        dex.add_rate('C', 'D', 0.2);
-        dex.add_rate('D', 'F', 2.5);
-
-        let src = 'A'.into();
-        let dst = 'D'.into();
-        let rate = dex.find_best_rate(&src, &dst);
-        assert_eq!(rate, Some(0.056));
-    }
-
-    #[test]
-    fn test_loop() {
-        let mut dex = Dex::new();
-        dex.add_rate('A', 'B', 1.4);
-        dex.add_rate('A', 'C', 0.1);
-        dex.add_rate('B', 'C', 0.2);
-        dex.add_rate('C', 'D', 0.2);
-        dex.add_rate('D', 'F', 2.5);
-
-        let src = 'D'.into();
-        let dst = 'F'.into();
-        let rate = dex.find_best_rate(&src, &dst);
-        assert_eq!(rate, Some(2.5));
     }
 }
 
